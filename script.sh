f6000f098fd6ece85ec42ee61b210957ab4c5b6f
Rajith <rajith.v@iinerds.com>
2018-11-28 12:43:12 +0530

    script added

--- script.sh
+++ script.sh
@@ -2,9 +2,10 @@
 utility_build () {
 cd utility
 source /etc/profile.d/maven.sh
-mvn package
+mvn package -DskipTests=true
 mvn install:install-file -Dfile=target/utility-1.0.0.jar -DgroupId=com.cct -DartifactId=utility -Dversion=1.0.0 -Dpackaging=jar
 echo "Utility build completed"
+echo "============================================================================"
 cd ..
 }
 utility_build
@@ -14,13 +15,16 @@ lambda_build () {
     cp ../utility/target/utility-1.0.0.jar .
     source /etc/profile.d/maven.sh
     mvn package -DskipTests=true
-    echo "$1 buid with utility completed"
+    echo "$1 build with utility completed"
+    echo "========================================================================"
     cd ..
 }
 
 for name in `cat lambdalist.txt`
 do 
-   echo " Calling Lambda Function"
+   echo "========================================================================="
+   echo " Calling $name"
+   echo "========================================================================="
    lambda_build $name 
 done
 
